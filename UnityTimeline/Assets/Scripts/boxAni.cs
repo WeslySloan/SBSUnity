0bf27431fcc9edb941353616d63864d0084848a9
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-12-03 23:28:36 +0900

    (2024.12.03) update

--- UnityTimeline/Assets/Scripts/boxAni.cs
+++ UnityTimeline/Assets/Scripts/boxAni.cs
@@ -11,7 +11,7 @@ public class TestAni : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // animator 변수에 (나 자신의 스크립트 오브젝트의 위치)컴포넌트를 찾아서 넣어라.
+        // animator 변수에 (나 자신의 스크립트 오브젝트으 위치)컴포넌트를 찾아서 넣어라.
         animator = GetComponent<Animator>();
     }
 
@@ -30,4 +30,3 @@ public class boxAni : TestAni
 {
     
 }
-
